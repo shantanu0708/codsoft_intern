c2e40cf6aaa05bec6f99e16f01c989b064b67c6c
Shanty0708 <shantanup6172@gmail.com>
2023-11-29 10:35:21 +0530

    the number game is completed

--- Number_game.java
+++ Number_game.java
@@ -1,57 +1,59 @@
 import java.util.Scanner;
 
-public class Number_game {
-     
-    public static int Guessing_number(int n){
-        Scanner sc = new Scanner(System.in);
-        int i=0;
-        while(i<7){
-            
-            System.out.println("Guess the number:");
-            int n1 = sc.nextInt();
-            if(n1 == n){
-                System.out.println("Congratulations, you guessed the number!");
-                break;
-            }
-            else if(n1<n){
-                System.out.println("The guess number is too low");
-            }else if(n1>n){
-                System.out.println("The guess number is too high");
-            }
-            i++;
-        }
-        sc.close();
-        if(i == 7){
-            System.out.println("You have exhausted the trials");
-            return 0;
-        }else if(i<7){
-            return 1;
-        }
-        return 0;
-    }
+public class Number_game { 
     public static void main(String[] args){
-        int guess = 0, hsc = 0, j=0;
-        Scanner sc1 = new Scanner(System.in);
-        while(j<5){
-            
-            System.out.println("*".repeat(45));
-            System.out.println("\t\tNumber Game");
-            System.out.println("*".repeat(45));
-            int num = 1 + (int) (Math.random() * 100);
-            guess = Guessing_number(num);
-            System.out.println("*".repeat(5)+" The original number is: "+num+" "+"*".repeat(5)+"\n");
-            if(guess == 1){
-                hsc = hsc + 100;
-            }
-            System.out.println("Your score is: "+hsc+"\n");
-            System.out.print("Ready To play?:(1 for yes/0 for no)  ");
-            int playagain = sc1.nextInt();
-            if(playagain == 0){
+        int guess = 0, hsc = 0;
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("Options:\n1.number game\n2.Score\n3.Exit\nEnter the option:");
+            int opt = sc.nextInt(); 
+            if(opt == 3){
                 break;
             }
-            j++;
-        } 
-        sc1.close(); 
+            switch (opt) {
+                case 1:
+                    System.out.println("*".repeat(45));
+                    System.out.println("\t\tNumber Game");
+                    System.out.println("*".repeat(45));
+                    int n = 1 + (int) (Math.random() * 100);
+                    System.out.println(" !!!Enter the number between 1 to 100!!! \n");      
+                    int i=0;
+                    while(i<6){
+                        System.out.println("Guess the number:");
+                        int n1 = sc.nextInt();
+                        if(n1 == n){
+                            System.out.println("Congratulations, you guessed the number!");
+                            break;
+                        }
+                        else if(n1<n){
+                            System.out.println("The guess number is too low");
+                        }else if(n1>n){
+                            System.out.println("The guess number is too high");
+                        }
+                        i++;
+                    }
+                    if(i == 6){
+                        System.out.println("You have exhausted the trials");
+                        guess = 0;
+                    }else if(i<6){
+                        guess = 1;
+                    }
+                    System.out.println("*".repeat(5)+" The original number is: "+n+" "+"*".repeat(5)+"\n");
+                    if(guess == 1){
+                    hsc = hsc + 100;
+                    }
+                    System.out.println("Your score is: "+hsc+"\n");
+                    break;
+                case 2:    
+                    System.out.println("*".repeat(45));
+                    System.out.println("     Your total score is: "+hsc+"     \n");
+                    System.out.println("*".repeat(45));
+                    break;
+                default:
+                    System.out.println("!!!Wrong Option!!!");
+                    break;
+            }
+        }  sc.close();
         return;
     }  
 
