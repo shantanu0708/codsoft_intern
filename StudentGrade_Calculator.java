d27f52dc1e4bf26578e1d4496f3be76060d35449
Shanty0708 <shantanup6172@gmail.com>
2023-11-30 10:30:15 +0530

    Begin

---
+++ StudentGrade_Calculator.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class StudentGrade_Calculator {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("_".repeat(50)+"\n");
+        System.out.println("\t    Student Grade Calculator");
+        System.out.println("_".repeat(50));
+        sc.close();
+    }
+}
