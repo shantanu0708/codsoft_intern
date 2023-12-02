b48f41cc71d8222ff6623a31304dc43993ace2b9
Shanty0708 <shantanup6172@gmail.com>
2023-12-02 10:03:02 +0530

    completed

--- ATMInterface.java
+++ ATMInterface.java
@@ -1,43 +1,53 @@
 import javax.swing.*;
-
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 class ATMInterface1 extends JFrame {
     private JTextField user;
-    private JLabel balanceLabel;
     private double balance = 1000.0;
+
     public ATMInterface1(){
         setTitle("ATM Interface");
-        setSize(800,600);
+        setLocation(360, 150);
+        setSize(800,400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         JPanel pan1 = new JPanel();
         add(pan1);
         place_components(pan1);
         setVisible(true);
     }
-        private void place_components(JPanel pan1) {
+    private void place_components(JPanel pan1) {
         pan1.setLayout(null);
+        
+        JLabel atmlabel1 = new JLabel("ATM MACHINE");
+        atmlabel1.setBounds(270, 10, 620, 100);
+        atmlabel1.setFont(new Font(("Aerial"),Font.BOLD,40));
+        pan1.add(atmlabel1);
+
         JLabel userLabel = new JLabel("Enter Amount:");
-        userLabel.setBounds(10, 20, 120, 25);
+        userLabel.setBounds(230, 150, 120, 30);
+        userLabel.setFont(new Font(("Aerial"),Font.PLAIN,16));
         pan1.add(userLabel);
 
         user = new JTextField(20);
-        user.setBounds(140, 20, 120, 25);
+        user.setBounds(340, 150, 120, 30);
         pan1.add(user);
 
         JButton withdrawButton = new JButton("Withdraw");
-        withdrawButton.setBounds(10, 80, 120, 25);
+        withdrawButton.setBounds(50, 250, 175, 50);
+        withdrawButton.setFont(new Font(("Aerial"),Font.CENTER_BASELINE,18));
         pan1.add(withdrawButton);
 
         JButton depositButton = new JButton("Deposit");
-        depositButton.setBounds(140, 80, 120, 25);
+        depositButton.setBounds(290, 250, 175, 50);
+        depositButton.setFont(new Font(("Aerial"),Font.CENTER_BASELINE,18));
         pan1.add(depositButton);
 
-        balanceLabel = new JLabel("Balance: $" + balance);
-        balanceLabel.setBounds(10, 120, 200, 25);
-        pan1.add(balanceLabel);
+        JButton balanceButton = new JButton("Check Balance");
+        balanceButton.setBounds(530,250,175,50);
+        balanceButton.setFont(new Font(("Aerial"),Font.CENTER_BASELINE,18));
+        pan1.add(balanceButton);
 
         withdrawButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -45,7 +55,6 @@ class ATMInterface1 extends JFrame {
                     double amount = Double.parseDouble(user.getText());
                     if (amount > 0 && amount <= balance) {
                         balance -= amount;
-                        updateBalanceLabel();
                         JOptionPane.showMessageDialog(null, "Withdrawn: $" + amount);
                     } else {
                         JOptionPane.showMessageDialog(null, "Invalid amount or insufficient funds.");
@@ -56,13 +65,13 @@ class ATMInterface1 extends JFrame {
             }
         }
         );
+
         depositButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 try {
                     double amount = Double.parseDouble(user.getText());
                     if (amount > 0) {
                         balance += amount;
-                        updateBalanceLabel();
                         JOptionPane.showMessageDialog(null, "Deposited: $" + amount);
                     } else {
                         JOptionPane.showMessageDialog(null, "Please enter a valid deposit amount.");
@@ -73,21 +82,25 @@ class ATMInterface1 extends JFrame {
             }
         }
         );
-    }
-    private void updateBalanceLabel() {
-        balanceLabel.setText("Balance: $" + balance);
+
+        balanceButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Available Balance: "+ balance);
+            }
+        }
+        );
     }
 }
+
 public class ATMInterface extends JFrame {
     public ATMInterface() {
         setTitle("ATM Machine");
+        setLocation(250,0);
         setSize(1024,768);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         JPanel pan = new JPanel();
         add(pan);
         placeComponents(pan);
-
         setVisible(true);
     }
     private void placeComponents(JPanel pan) {
@@ -104,13 +117,13 @@ public class ATMInterface extends JFrame {
         pan.add(startButton);
 
         startButton.addActionListener( new ActionListener() {
-            public void actionPerformed(ActionEvent e){   
+            public void actionPerformed(ActionEvent e){ 
+                setVisible(false);  
                 new ATMInterface1(); 
            } 
         }
         );
     }
-
     public static void main(String[] args) {
         new ATMInterface();
     }   
