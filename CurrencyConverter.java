c9aa21ccf1772323e7aa3abd2b37768316c8b2dc
Shanty0708 <shantanup6172@gmail.com>
2023-12-16 15:01:31 +0530

    modified

---
+++ CurrencyConverter.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+class CurrencyConverter {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String sourceCurrency,targetCurrency;
+        double amount,convertedAmt;
+        Map<String, Double> exchangeRates = new HashMap<>();
+        exchangeRates.put("USD", 1.0);
+        exchangeRates.put("EUR", 0.92);
+        exchangeRates.put("GBP", 0.79);
+        exchangeRates.put("JPY", 142.12);
+        exchangeRates.put("INR", 83.04);
+
+        System.out.print("Enter source currency (USD, EUR, GBP, JPY, INR): ");
+        sourceCurrency = sc.next().toUpperCase();
+
+        System.out.print("Enter target currency (USD, EUR, GBP, JPY, INR): ");
+        targetCurrency = sc.next().toUpperCase();
+
+        System.out.print("Enter amount to convert: ");
+        amount = sc.nextDouble();
+
+        convertedAmt = convertCurrency(amount, exchangeRates.get(targetCurrency) / exchangeRates.get(sourceCurrency));
+
+        System.out.println("Converted amount: " + convertedAmt + " " + targetCurrency);
+        sc.close();
+    }
+    private static double convertCurrency(double amt, double exchangeRate) {
+        return amt * exchangeRate;
+    }
+}
